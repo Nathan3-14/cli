332be65838aae41a7177fe9e69a95dd31291b4a2
Nathan3-14 <nathan.watson.172@gmail.com>
2024-03-31 18:27:15 +0100

    Improved

--- custom/cli.py
+++ custom/cli.py
@@ -1,5 +1,5 @@
 import sys
-from typing import Any, Dict, Tuple
+from typing import Any, Dict, List, Tuple
 from rich.console import Console
 
 class CLI:
@@ -10,6 +10,21 @@ class CLI:
 class Command:
     def __init__(self, desired_args: Dict[str, Tuple[Any, Any]]) -> None:
         self.desired_args = desired_args
+        self.desired_args_keys = list(self.desired_args)
+    
+    def run(self, input_args: List[str]) -> None:
+        if not self.check_args(input_args):
+            quit()
+
+    def check_args(self, input_args: List[str]) -> bool:
+        for index, arg in enumerate(input_args):
+            try:
+                arg = self.desired_args[
+                    self.desired_args_keys[index]
+                ][0](arg)
+            except ValueError:
+                return False
+        return True
 
 if __name__ == "__main__":
     add_d_args = {
